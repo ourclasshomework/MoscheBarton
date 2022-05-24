9253f9add507c3357b0c012c7cde0fb38064b414
ourclasshomework <53077568+ourclasshomework@users.noreply.github.com>
2022-05-25 02:18:39 +0800

    0525_2

--- OLD/Test/Test/Test/Form1.vb
+++
@@ -1,7 +0,0 @@
-﻿Public Class Form1
-    Private Sub Form1_MouseMove(sender As Object, e As MouseEventArgs) Handles MyBase.MouseMove
-        Label1.Text = Str(e.X) + ", " + Str(e.Y)
-        PictureBox1.Left = e.X
-        PictureBox1.Top = e.Y
-    End Sub
-End Class
